a18f6bca16c0b3a0f20f4ef35968c0beafc956f5
Sorx4s <dgantti@outlook.com>
2026-08-14 23:11:23 -0600

    chore: elimina archivos duplicados del front e inecesarios del back

--- 2-backend-java/src/main/java/com/equipo26/financeai/model/enums/FinancialProfile.java
+++
@@ -1,7 +0,0 @@
-package com.equipo26.financeai.model.enums;
-
-public enum FinancialProfile {
-    SALUDABLE,
-    EN_OBSERVACION,
-    EN_RIESGO
-}
--- 2-backend-java/src/main/java/com/equipo26/financeai/repository/FinancialRepository.java
+++
@@ -1,6 +0,0 @@
-package com.equipo26.financeai.repository;
-import com.equipo26.financeai.model.FinancialAnalysis;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface FinancialRepository extends JpaRepository<FinancialAnalysis, Long> {
-}
